36f211581f25d494d73b7f196fb3d1208ea02bb2
PradnyaPhule <82202444+PradnyaPhule@users.noreply.github.com>
2021-05-24 10:39:40 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,4 +1,9 @@
-resource "aws_instance" "example" {
-  ami           = "ami-2757f631"
-  instance_type = "t2.micro"
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "demo-vpc"
+    Purpose = "Jenkins Demo"
+  }
 }
